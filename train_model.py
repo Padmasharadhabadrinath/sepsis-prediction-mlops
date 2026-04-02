90f02916ca5ba04c917229736272ba0baf3b464c
Padmasharadha0523 <padmasharadha6@gmail.com>
2026-04-02 12:43:14 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -14,6 +14,12 @@ def train_model(df, scaler):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
+    from imblearn.over_sampling import SMOTE
+
+    smote = SMOTE(random_state=42)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
+
+    print("After SMOTE:", X_train.shape, y_train.shape)
 
     # ✅ Scaling (correct now)
     scaler = StandardScaler()
@@ -28,7 +34,7 @@ def train_model(df, scaler):
 
     # ✅ FIXED models dictionary
     models = {
-        "RandomForest": RandomForestClassifier(n_estimators=300, random_state=42),
+        "RandomForest": RandomForestClassifier(n_estimators=300, random_state=42,class_weight='balanced'),
 
         "Bagging": BaggingClassifier(
             estimator=DecisionTreeClassifier(class_weight='balanced'),
